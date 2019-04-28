803096059d32ef498409f38e4de0e3e1552f660e
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2019-04-28 01:09:39 -0400

    Fixed issue with x and y spaces

--- src/app/global/global.component.ts
+++ src/app/global/global.component.ts
@@ -5,12 +5,16 @@ import { Component, Input } from '@angular/core';
   template: `
     <div *ngIf="showMargin">
       <h1>Margin</h1>
-      <div *ngFor="let marg of margins">
-        -- {{ marg }} --
+      <div *ngFor="let marg of margins" class="margins">
         <div>
+          <span>-</span>
+          <br />
           <span>|</span>
           <span class="{{ marg }}">X</span>
           <span>|</span>
+          {{ marg }}
+          <br />
+          <span>-</span>
         </div>
       </div>
     </div>
@@ -33,5 +37,35 @@ export class GlobalComponent {
     'rd-m-r-md',
     'rd-m-r-lg',
     'rd-m-r-vl',
+
+    'rd-m-x-vs',
+    'rd-m-x-sm',
+    'rd-m-x-md',
+    'rd-m-x-lg',
+    'rd-m-x-vl',
+
+    'rd-m-t-vs',
+    'rd-m-t-sm',
+    'rd-m-t-md',
+    'rd-m-t-lg',
+    'rd-m-t-vl',
+
+    'rd-m-b-vs',
+    'rd-m-b-sm',
+    'rd-m-b-md',
+    'rd-m-b-lg',
+    'rd-m-b-vl',
+
+    'rd-m-y-vs',
+    'rd-m-y-sm',
+    'rd-m-y-md',
+    'rd-m-y-lg',
+    'rd-m-y-vl',
+
+    'rd-m-a-vs',
+    'rd-m-a-sm',
+    'rd-m-a-md',
+    'rd-m-a-lg',
+    'rd-m-a-vl',
   ];
 }
